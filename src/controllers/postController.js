f7e52333b55782edc55586b8dc407da83d121ce8
JYPIT <jypit@naver.com>
2022-08-26 19:12:22 +0900

    mixin post_info 출력

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -4,11 +4,33 @@ const fakeUser = {
 };
 
 export const trending = (req, res) => {
-  const posts = [1, 2, 3, 4, 5];
+  const posts = [
+    {
+      title: "지구서 100광년 거리에 있는 ‘바다행성’ 발견",
+      createdAt: "1시간 전",
+      views: 777,
+      comments: 231,
+      id: 1,
+    },
+    {
+      title: "메타버스 윤리 원칙, 시장 발전 막을까?…자율 규제 기반 윤리 원칙 적용에...",
+      createdAt: "13분 전",
+      views: 134,
+      comments: 32,
+      id: 2,
+    },
+    {
+      title: "700광년 밖 가스행성서 CO₂첫 포착",
+      createdAt: "27분 전",
+      views: 343,
+      comments: 111,
+      id: 3,
+    },
+  ];
   return res.render("home", { pageTitle: "Ἀγορά", fakeUser: fakeUser, posts: posts });
 };
 export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
-export const edit = (req, res) => res.render("edit");
+export const editPost = (req, res) => res.render("editPost", { pageTitle: "Edit Post", fakeUser });
 export const search = (req, res) => res.send("Search");
 export const deletePost = (req, res) => res.send("Delete Post");
 export const upload = (req, res) => res.send("Upload");
--- src/routers/postRouter.js
+++ src/routers/postRouter.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { see, edit, deletePost, upload } from "../controllers/postController.js";
+import { see, editPost, deletePost, upload } from "../controllers/postController.js";
 
 const postRouter = express.Router();
 
 postRouter.get("/upload", upload);
 postRouter.get("/:id(\\d+)", see);
-postRouter.get("/:id(\\d+)/edit", edit);
+postRouter.get("/:id(\\d+)/edit", editPost);
 postRouter.get("/:id(\\d+)/delete", deletePost);
 
 export default postRouter;
